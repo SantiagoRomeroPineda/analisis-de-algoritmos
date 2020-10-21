6f1eea845966c7215b4ab452293483fa12c1d34c
Santiago <saropi9978@gmail.com>
2020-10-21 07:59:22 -0500

    imprimo matriz

--- taller3/cutting_cloth.py
+++ taller3/cutting_cloth.py
@@ -1,3 +1,4 @@
+import numpy
 def cortes(x=3,y=5):
     matriz=[[0 for j in range(x+1)]for i in range(y+1)]
     for i in range(1,y+1):
@@ -9,7 +10,7 @@ def cortes(x=3,y=5):
                 for k in range(1,i):
                     respuesta=min(respuesta, 1 + matriz[i-k][j] + matriz[k][j])
                 matriz[i][j]=respuesta
-    
+    print(numpy.array(matriz))
     return matriz[y][x]
 
 print(cortes())
